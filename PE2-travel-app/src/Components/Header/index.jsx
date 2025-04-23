fdf122f26dc0e9bb952ca0dbebf40a0be3f5bce1
Leanne <lemeyer.no@gmail.com>
2025-04-23 20:55:53 +0200

    re-installed react-router-dom

---
+++ PE2-travel-app/src/Components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../Assets/Images/logo.png';
+
+function Header(){
+  return(
+    <div>
+     
+   <header>
+   <Logo/>
+
+
+   </header>
+
+   </div>
+
+  )
+}
+
+export default Header;
