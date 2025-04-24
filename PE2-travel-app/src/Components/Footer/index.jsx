70993af3f9788aa45a8d679a0263679bf40ded72
Leanne <lemeyer.no@gmail.com>
2025-04-24 11:48:01 +0200

    Created footer component and added to layout

---
+++ PE2-travel-app/src/Components/Footer/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Register from '../../assets/Images/Register.png';
+import Login from '../../assets/Images/Login.png';
+import Facebook from '../../assets/Images/Facebook.png';
+import Twitter from '../../assets/Images/Twitter.png';
+import Instagram from '../../assets/Images/Instagram.png';
+
+function Footer() {
+    return(
+    <div>
+        <footer>    
+<p>© 2025 Holidaze  </p>
+<a href="/Register">
+<img src={Register} alt="Register icon" />
+Register
+</a>
+<a href="/Login">
+<img src={Login} alt="Login icon" />
+Login
+</a>
+
+<div>
+<img src={Facebook} alt="Facebook icon" />
+<img src={Instagram} alt="Instagram icon" />
+<img src={Twitter} alt="Twitter icon" />
+</div>
+        </footer>
+    </div> ) }
+
+    export default Footer;
--- PE2-travel-app/src/Components/Layout/index.jsx
+++ PE2-travel-app/src/Components/Layout/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
 import Header from "../Header";
-
-// import Footer from "../Footer";
+import Footer from "../Footer";
 
 
 /**
@@ -21,7 +20,7 @@ return(
  
   <Outlet/>
   
-  {/* <Footer/> */}
+  <Footer/>
  
   </div>
  
