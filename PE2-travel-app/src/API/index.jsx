f2ae7bf014e262c4047878c9beb988d5fc5f50fe
Leanne <lemeyer.no@gmail.com>
2025-04-24 15:13:11 +0200

    added jsdocs to FetchStays component

--- PE2-travel-app/src/API/index.jsx
+++ PE2-travel-app/src/API/index.jsx
@@ -7,6 +7,17 @@ import React from 'react';
 
 const url = "https://v2.api.noroff.dev/holidaze/venues";
 
+/**
+ * FetchStays - fetches and displays venue (stays) data from an API.
+ *
+ * This component uses the Fetch API to retrieve venue (stays) data from a specified URL and
+ * stores it in a local state. It also uses a custom store (useMyStore) to manage the
+ * state of the stays data. The fetched data is then passed to the DisplayStays component
+ *
+ * @component
+ * @returns {JSX.Element} The FetchStays component that displays the fetched stays data.
+ */
+
 function FetchStays() {
 
   const setStoreStays = useMyStore((state) => state.setStays);
