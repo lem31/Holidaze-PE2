e5f64797afbaa5aa5188825b7876cec2cccb6b3d
Leanne <lemeyer.no@gmail.com>
2025-04-29 13:59:43 +0200

    created createvenueform component

---
+++ PE2-travel-app/src/Components/CreateVenueForm/index.jsx
@@ -0,0 +1,233 @@
+import Pets from '../../assets/Images/Pets.png';
+import Breakfast from '../../assets/Images/Breakfast.png';
+import Parking from '../../assets/Images/Parking.png';
+import Wifi from '../../assets/Images/Wifi.png';
+
+
+
+const CreateVenueForm = ({ formValues, onInputChange, onImageChange, onAddImage, onSubmit, validationErrors= {}, }) => {
+    return (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit(e);
+        }}
+      >
+        <TextField
+          label="Name"
+          name="name"
+          value={formValues.name}
+          onChange={(e) => onInputChange(e.target.name, e.target.value)}
+          variant="outlined"
+          fullWidth
+          required
+          sx={{ marginBottom: 2 }}
+        />
+          {validationErrors.name && <p>{validationErrors.name}</p>}
+    
+
+        <TextField
+          label="Description"
+          name="description"
+          value={formValues.description}
+            onChange={(e) => onInputChange(e.target.name, e.target.value)}
+            variant="outlined"
+            fullWidth
+            required
+            sx={{ marginBottom: 2 }}
+        />
+          {validationErrors.description && <p>{validationErrors.description}</p>}
+      
+     
+        {formValues.images.map((image, index) => (
+          <Box key={index} sx={{ marginBottom: 2 }}>
+            <TextField
+              label="Image URL"
+              name="url"
+              value={image.url}
+              onChange={(e) => onImageChange(index, e.target.name, e.target.value)}
+              variant="outlined"
+              fullWidth
+              sx={{ marginBottom: 1 }}
+            />
+              {validationErrors.url && <p>{validationErrors.url}</p>}
+            <TextField
+              label="Alt Text"
+              name="alt"
+              value={image.alt}
+              onChange={(e) => onImageChange(index, e.target.name, e.target.value)}
+              variant="outlined"
+              fullWidth
+            />
+              {validationErrors.alt && <p>{validationErrors.alt}</p>}
+          </Box>
+        ))}
+        <Button
+          onClick={onAddImage}
+          type="button"
+          variant="outlined"
+          fullWidth
+          sx={{ marginBottom: 2 }}
+        >
+          Add Another Image
+        </Button>
+
+
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+      Register
+        </Button>
+   <TextField
+          label="price"
+          name="price"
+          value={formValues.price}
+            onChange={(e) => onInputChange(e.target.name, e.target.value)}
+            variant="outlined"
+            fullWidth
+            required
+            sx={{ marginBottom: 2 }}
+        />
+          {validationErrors.price && <p>{validationErrors.price}</p>}
+          <TextField
+          label="maxGuests"
+          name="maxGuests"
+          value={formValues.maxGuests}
+            onChange={(e) => onInputChange(e.target.name, e.target.value)}
+            variant="outlined"
+            fullWidth
+            required
+            sx={{ marginBottom: 2 }}
+        />
+          {validationErrors.maxGuests && <p>{validationErrors.maxGuests}</p>}
+          <TextField
+          label="Rating"
+          name="rating"
+          value={formValues.rating}
+            onChange={(e) => onInputChange(e.target.name, e.target.value)}
+            variant="outlined"
+            fullWidth
+            required
+            sx={{ marginBottom: 2 }}
+        />
+          {validationErrors.rating && <p>{validationErrors.rating}</p>}
+    <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: 2 }}>
+        <Button
+      
+            variant={formValues.parking ? "contained" : "outlined"}
+            onClick={() => onInputChange("parking", !formValues.parking)}
+            startIcon={<img src={Parking} alt="Parking" style={{ width: 24, height: 24 }} />}
+        >
+              <img src={Parking} alt="Parking" />
+            Parking
+        </Button>
+        <Button
+            variant={formValues.wifi ? "contained" : "outlined"}
+            onClick={() => onInputChange("wifi", !formValues.wifi)}
+            startIcon={<img src={Wifi} alt="Wifi" style={{ width: 24, height: 24 }} />}
+        >
+              <img src={Wifi} alt="Wifi" />
+            Wifi
+        </Button>
+        <Button
+            variant={formValues.breakfast ? "contained" : "outlined"}
+            onClick={() => onInputChange("breakfast", !formValues.breakfast)}
+            startIcon={<img src={Breakfast} alt="Breakfast" style={{ width: 24, height: 24 }} />}
+        >
+              <img src={Breakfast} alt="Breakfast" />
+            Breakfast
+        </Button>
+        <Button
+            variant={formValues.pets ? "contained" : "outlined"}
+            onClick={() => onInputChange("pets", !formValues.pets)}
+            startIcon={<img src={Pets} alt="Pets" style={{ width: 24, height: 24 }} />}
+        >
+              <img src={Pets} alt="Pets" />
+            Pets
+        </Button>
+    </Box>
+    <Box sx={{ marginBottom: 2 }}>
+        <TextField
+            label="Address"
+            name="location.address"
+            value={formValues.location?.address || ''}
+            onChange={(e) => onInputChange(e.target.name, e.target.value)}
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
+        />
+        {validationErrors['location.address'] && <p>{validationErrors['location.address']}</p>}
+
+        <TextField
+            label="City"
+            name="location.city"
+            value={formValues.location?.city || ''}
+            onChange={(e) => onInputChange(e.target.name, e.target.value)}
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
+        />
+        {validationErrors['location.city'] && <p>{validationErrors['location.city']}</p>}
+
+        <TextField
+            label="Zip"
+            name="location.zip"
+            value={formValues.location?.zip || ''}
+            onChange={(e) => onInputChange(e.target.name, e.target.value)}
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
+        />
+        {validationErrors['location.zip'] && <p>{validationErrors['location.zip']}</p>}
+
+        <TextField
+            label="Country"
+            name="location.country"
+            value={formValues.location?.country || ''}
+            onChange={(e) => onInputChange(e.target.name, e.target.value)}
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
+        />
+        {validationErrors['location.country'] && <p>{validationErrors['location.country']}</p>}
+
+        <TextField
+            label="Continent"
+            name="location.continent"
+            value={formValues.location?.continent || ''}
+            onChange={(e) => onInputChange(e.target.name, e.target.value)}
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
+        />
+        {validationErrors['location.continent'] && <p>{validationErrors['location.continent']}</p>}
+
+        <TextField
+            label="Latitude"
+            name="location.lat"
+            type="number"
+            value={formValues.location?.lat || 0}
+            onChange={(e) => onInputChange(e.target.name, parseFloat(e.target.value))}
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
+        />
+        {validationErrors['location.lat'] && <p>{validationErrors['location.lat']}</p>}
+
+        <TextField
+            label="Longitude"
+            name="location.lng"
+            type="number"
+            value={formValues.location?.lng || 0}
+            onChange={(e) => onInputChange(e.target.name, parseFloat(e.target.value))}
+            variant="outlined"
+            fullWidth
+            sx={{ marginBottom: 2 }}
+        />
+        {validationErrors['location.lng'] && <p>{validationErrors['location.lng']}</p>}
+    </Box>
+      
+      </form>
+    );
+  };
+  
+  export default CreateVenueForm;
+  
