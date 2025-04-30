0e14ff8303d934765807437197661c848cb6302d
Leanne <lemeyer.no@gmail.com>
2025-04-30 14:04:05 +0200

    updated fetchUserProfile

--- PE2-travel-app/src/API/FetchUserProfile/index.js
+++ PE2-travel-app/src/API/FetchUserProfile/index.js
@@ -2,21 +2,19 @@ async function fetchUserProfile(userName, token) {
  if(!token){
     throw new Error('User not authenticated');
  }
-
+ const apiKey = import.meta.env.API_KEY;
  try{
     console.log('Fetching user profile for:', userName);
-    console.log('Using token:', token);
     const response = await fetch(`https://v2.api.noroff.dev/holidaze/profiles/${userName}`, {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${token}`,
-            'X-Noroff-API-Key': 'f920c7be-b352-412a-bfe3-67cf36aebe41',
+            'X-Noroff-API-Key': apiKey,
         },
     });
 
     if(!response.ok){
         console.error('Failed API Response:', response);
-        console.error('API Key Used:', process.env.API_KEY);
         throw new Error('Failed to fetch user profile');
     }
 
