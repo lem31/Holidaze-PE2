471a26ad3e68fcbba4c484f2a9980cfab3dba022
Leanne <lemeyer.no@gmail.com>
2025-05-05 11:12:38 +0200

    created function for updating user profile

---
+++ PE2-travel-app/src/API/UpdateProfile/index.js
@@ -0,0 +1,25 @@
+async function updateProfile(endpoint, userData) {
+    try {
+      const response = await fetch(endpoint, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`, 
+          "X-Noroff-API-Key": "f920c7be-b352-412a-bfe3-67cf36aebe41",
+        },
+        body: JSON.stringify(userData),
+      });
+  
+      if (!response.ok) {
+        throw new Error("Failed to update profile");
+      }
+  
+      const data = await response.json();
+      console.log("Profile Successfully updated:", data);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  export default updateProfile;
+  
