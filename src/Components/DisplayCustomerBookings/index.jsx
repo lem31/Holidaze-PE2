ccc8c9d6a3a585155a6905c5dbcdb4eb19b2feab
Leanne <lemeyer.no@gmail.com>
2025-05-23 21:03:21 +0200

    added remaining styles to customer bookings page

--- src/Components/DisplayCustomerBookings/index.jsx
+++ src/Components/DisplayCustomerBookings/index.jsx
@@ -1,81 +1,113 @@
 import { useEffect, useState } from "react";
 import fetchCustomerBookings from "../../API/FetchCustomerBookings";
-import cBookingStyles from '../../CSS_Modules/CustomerBookings/customerBookings.module.css';
-import gStyles from '../../CSS_Modules/Global/global.module.css';
+import cBookingStyles from "../../CSS_Modules/CustomerBookings/customerBookings.module.css";
+import gStyles from "../../CSS_Modules/Global/global.module.css";
 
-const DisplayCustomerBookings = () => { 
-    const [bookingsData, setBookingsData] = useState([]);
-    const [error, setError] = useState(null);
+const DisplayCustomerBookings = () => {
+  const [bookingsData, setBookingsData] = useState([]);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchBookings = async () => {
-            try {
-                const bookings = await fetchCustomerBookings();
-                setBookingsData(bookings);
-            } catch (error) {
-                console.error("Error fetching customer bookings:", error);
-                setError("Error fetching customer bookings");
-            }
-        };
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const bookings = await fetchCustomerBookings();
+        setBookingsData(bookings);
+      } catch (error) {
+        console.error("Error fetching customer bookings:", error);
+        setError("Error fetching customer bookings");
+      }
+    };
 
-        fetchBookings();
-    }, []);
+    fetchBookings();
+  }, []);
 
-    if (error) {
-        return <div>{error}</div>;
-    }
-    if (!bookingsData || bookingsData.length === 0) {
-        return <div>No bookings found</div>;
-    }
+  if (error) {
+    return <div>{error}</div>;
+  }
+  if (!bookingsData || bookingsData.length === 0) {
+    return <div>No bookings found</div>;
+  }
 
-    return (
-        <div>
-            <div className={cBookingStyles.h1Div}>
-            <h1 className={`${gStyles.h1Black}${cBookingStyles.h1}`}>Bookings</h1>
+  return (
+    <div>
+      <div className={cBookingStyles.h1Div}>
+        <h1 className={`${gStyles.h1Black}${cBookingStyles.h1}`}>Bookings</h1>
+      </div>
+      {bookingsData.map((booking) => {
+        const numberOfNights =
+          (new Date(booking.dateTo) - new Date(booking.dateFrom)) /
+          (1000 * 60 * 60 * 24);
+        const totalPrice = booking.venue?.price
+          ? booking.venue.price * numberOfNights
+          : "N/A";
+
+        return (
+          <div className={cBookingStyles.outerContainer}>
+            <div className={cBookingStyles.innerContainer}>
+              <div className={cBookingStyles.cardLayout} key={booking.id}>
+                <div className={cBookingStyles.nameCreatedImgDiv}>
+                  <div className={cBookingStyles.nameCreatedDiv}>
+                    <h2 className={`${gStyles.h2White} ${cBookingStyles.h2}`}>
+                      {booking.venue?.name || "Venue Not Found"}
+                    </h2>
+                    <p className={gStyles.bodyWhite}>
+                      Booked on:{new Date(booking.created).toLocaleDateString()}
+                    </p>
+                  </div>
+                  {booking.venue?.media?.[0]?.url ? (
+                    <img
+                      className={cBookingStyles.bookingImg}
+                      src={booking.venue.media[0].url}
+                      alt={booking.venue.name}
+                    />
+                  ) : (
+                    <p>No image available</p>
+                  )}
+                </div>
+                <div className={cBookingStyles.bookingInfoDiv}>
+                  <div className={cBookingStyles.bookingInfoGrid}>
+                    <p className={gStyles.bodyWhite}>Guests:</p>
+                    <p className={gStyles.bodyWhite}>Check-in:</p>
+                    <p className={gStyles.bodyWhite}>Check-out:</p>
+                    <p className={gStyles.bodyWhite}>No. of Nights:</p>
+                    <p className={gStyles.bodyWhite}>Total Price:</p>
+                    <p className={gStyles.bodyWhite}>{booking.guests}</p>
+                    <p className={gStyles.bodyWhite}>
+                      {new Date(booking.dateFrom).toLocaleDateString()}
+                    </p>
+                    <p className={gStyles.bodyWhite}>
+                      {new Date(booking.dateTo).toLocaleDateString()}
+                    </p>
+                    <p className={gStyles.bodyWhite}>{numberOfNights}</p>
+                    <p className={gStyles.bodyWhite}>{totalPrice}</p>
+                  </div>
+
+                  <div className={cBookingStyles.bookingInfoGridMobile}>
+                    <p className={gStyles.bodyWhite}>Guests:</p>
+                    <p className={gStyles.bodyWhite}>{booking.guests}</p>
+                    <p className={gStyles.bodyWhite}>Check-in:</p>
+                    <p className={gStyles.bodyWhite}>
+                      {new Date(booking.dateFrom).toLocaleDateString()}
+                    </p>
+                    <p className={gStyles.bodyWhite}>Check-out:</p>
+
+                    <p className={gStyles.bodyWhite}>
+                      {new Date(booking.dateTo).toLocaleDateString()}
+                    </p>
+                    <p className={gStyles.bodyWhite}>No. of Nights:</p>
+                    <p className={gStyles.bodyWhite}>{numberOfNights}</p>
+                    <p className={gStyles.bodyWhite}>Total Price:</p>
+
+                    <p className={gStyles.bodyWhite}>{totalPrice}</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            {bookingsData.map((booking) => {
-             
-                const numberOfNights = (new Date(booking.dateTo) - new Date(booking.dateFrom)) / (1000 * 60 * 60 * 24);
-                const totalPrice = booking.venue?.price ? booking.venue.price * numberOfNights : "N/A";
-
-                return (
-                    <div className={cBookingStyles.outerContainer} >
-                        <div className={cBookingStyles.innerContainer}>
-                        
-                    <div className={cBookingStyles.cardLayout} key={booking.id}>
-                        <div className={cBookingStyles.nameCreatedImgDiv}>
-                        <div className={cBookingStyles.nameCreatedDiv}>
-                        <h2 className={`${gStyles.h2White} ${cBookingStyles.h2}`}>{booking.venue?.name || "Venue Not Found"}</h2>
-                        <p className={gStyles.bodyWhite}>Booked on:{new Date(booking.created).toLocaleDateString()}</p>
-                        </div>
-                        {booking.venue?.media?.[0]?.url ? (
-                            <img className={cBookingStyles.bookingImg} src={booking.venue.media[0].url} alt={booking.venue.name} />
-                        ) : (
-                            <p>No image available</p>
-                        )}
-                        </div>
-                        <div className={cBookingStyles.bookingInfoDiv}>
-                           
-                        <div className={cBookingStyles.bookingInfoGrid}>
-                        <p className={gStyles.bodyWhite}>Guests:</p> 
-                        <p className={gStyles.bodyWhite}>Check-in:</p>
-                         <p className={gStyles.bodyWhite}>Check-out:</p>
-                         <p className={gStyles.bodyWhite}>No. of Nights:</p>  
-                            <p className={gStyles.bodyWhite}>Total Price:</p>
-                        <p className={gStyles.bodyWhite}>{booking.guests}</p>
-                        <p className={gStyles.bodyWhite}>{new Date(booking.dateFrom).toLocaleDateString()}</p>
-                        <p className={gStyles.bodyWhite}>{new Date(booking.dateTo).toLocaleDateString()}</p>
-                        <p className={gStyles.bodyWhite}>{numberOfNights}</p>
-                     <p className={gStyles.bodyWhite}>{totalPrice}</p>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default DisplayCustomerBookings;
