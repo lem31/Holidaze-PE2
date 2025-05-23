788bed03772ee7931ff8d1313f779e3670d64d6e
Leanne <lemeyer.no@gmail.com>
2025-05-23 10:25:01 +0200

    added styles to bookings section on vm profile page

--- src/Components/VMBookings/index.jsx
+++ src/Components/VMBookings/index.jsx
@@ -30,15 +30,42 @@ const nights = Math.max(1, Math.round((new Date(booking.dateTo) - new Date(booki
           <div className={vmProfileStyles.bookingCardWrapper} key={`${booking.id || `fallback`}-${index}`}>
             <div className={vmProfileStyles.bookingInfoDiv}>
           <div className={vmProfileStyles.venueNameImgDiv}>
-              <h2><strong>Venue Name:</strong> {venue?.name || "Unknown Venue"}</h2>
+              <h2 style={{marginBottom: '12px'}}className={gStyles.h2White}><strong>{venue?.name || "Unknown Venue"}</strong> </h2>
               <img src={venue?.media[0]?.url} alt="Venue" className={vmProfileStyles.bookingImage} />
               </div>
-              <p><strong>Customer</strong> {booking.customer.name}</p>
-                <p><strong>Total Guests</strong> {booking.guests}</p>
-              <p><strong>Date</strong> {booking.dateFrom} - {booking.dateTo}</p>
-        <p>Duration:{nights}nights</p>
-              <p><strong>Total Price</strong> {venue?.price * nights}</p>
-              <p><strong>Created:</strong> Booked on {booking.created}</p>
+              <div>
+                <div className={vmProfileStyles.bookingRowDiv}>
+              <p className={gStyles.bodyLPurple}><strong>Customer</strong></p>
+               <p className={gStyles.bodyWhite}> {booking.customer.name}</p>
+               </div>
+                <div className={vmProfileStyles.bookingRowDiv}>
+                <p className={gStyles.bodyLPurple}><strong>Total Guests</strong></p> 
+                <p className={gStyles.bodyWhite}>{booking.guests}</p>
+                 </div>
+                  <div className={vmProfileStyles.bookingRowDiv}>
+              <p className={gStyles.bodyLPurple}><strong>Check-in</strong></p> 
+              <p className={gStyles.bodyWhite}>{new Date(booking.dateFrom).toLocaleDateString()}</p> 
+              </div>
+              
+                <div className={vmProfileStyles.bookingRowDiv}>
+              <p className={gStyles.bodyLPurple}><strong>Check-out</strong></p> 
+              <p className={gStyles.bodyWhite}>{new Date(booking.dateTo).toLocaleDateString()}</p>
+              
+               </div>
+                 <div className={vmProfileStyles.bookingRowDiv}>
+        <p className={gStyles.bodyLPurple}>Duration</p>
+      
+          <p className={gStyles.bodyWhite}>{nights}/nights</p>
+            </div>
+ <div className={vmProfileStyles.bookingRowDiv}>
+              <p className={gStyles.bodyLPurple}><strong>Total Price</strong></p> 
+              <p className={gStyles.bodyWhite}>{venue?.price * nights}NOK</p>
+               </div>
+                <div className={vmProfileStyles.bookingRowDiv}>
+              <p className={gStyles.bodyLPurple}><strong>Booked on</strong></p> 
+              <p className={gStyles.bodyWhite}>{new Date(booking.created).toLocaleDateString()}</p>
+                </div>
+              </div>
             </div>
           </div>
         );
