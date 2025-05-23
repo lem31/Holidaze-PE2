46c8f1394c7d215dcf57427cf0fc80ee81e983f4
Leanne <lemeyer.no@gmail.com>
2025-05-23 09:48:41 +0200

    added styles to Bookings page

--- src/Components/VMBookings/index.jsx
+++ src/Components/VMBookings/index.jsx
@@ -18,34 +18,40 @@ function VMBookings() {
   console.log("Venue Bookings:", venueBookings);
   
   
-  return (
-    <div>
+ return (
+  <div>
     {venueBookings.length > 0 ? (
-      venueBookings.map((booking, index) => (
+      venueBookings.map((booking, index) => {
+       
+        const venue = vmVenues.find(v => v.bookings?.some(b => b.id === booking.id));
+    
+const nights = Math.max(1, Math.round((new Date(booking.dateTo) - new Date(booking.dateFrom)) / (1000 * 60 * 60 * 24)));
+        return (
+          <div className={vmProfileStyles.bookingCardWrapper} key={`${booking.id || `fallback`}-${index}`}>
+            <div className={vmProfileStyles.bookingInfoDiv}>
+          <div className={vmProfileStyles.venueNameImgDiv}>
+              <h2><strong>Venue Name:</strong> {venue?.name || "Unknown Venue"}</h2>
+              <img src={venue?.media[0]?.url} alt="Venue" className={vmProfileStyles.bookingImage} />
+              </div>
+              <p><strong>Customer</strong> {booking.customer.name}</p>
+                <p><strong>Total Guests</strong> {booking.guests}</p>
+              <p><strong>Date</strong> {booking.dateFrom} - {booking.dateTo}</p>
+        <p>Duration:{nights}nights</p>
+              <p><strong>Total Price</strong> {venue?.price * nights}</p>
+              <p><strong>Created:</strong> Booked on {booking.created}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <>
+        <h2 className={gStyles.h2White}>No Bookings Available</h2>
+        <p>Please check back later.</p>
+      </>
+    )}
+  </div>
+);
 
-        <div className={vmProfileStyles.bookingCardWrapper}>
-        <div className={vmProfileStyles.bookingInfoDiv} key={`${booking.id || `fallback`}-${index}`}>
-          <h2>Booking ID: {booking.id}</h2>
-          <p>Customer: {booking.customer.name}</p>
-          <p>Date: {booking.dateFrom} - {booking.dateTo}</p>
-          <p>Guests: {booking.guests}</p>
-          <p>Price: {booking.price}</p>
-          <p>Created: {booking.created}</p>
-        </div>
-        </div>
-          ))
-        ) : (
-          <>
-            <h2 className={gStyles.h2White}>No Bookings Available</h2>
-            <p>Please check back later.</p>
-          </>
-        )}
-
-
-
-        </div>
- 
-  );
 
 }
 
